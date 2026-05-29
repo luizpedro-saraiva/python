0d64543403ea126f1e114a634f11c159f6992bb0
luizsaraiva-coder <luizsaraiva@discente.ufg.br>
2026-05-29 20:14:40 -0300

    adicionando Desafio1 (devGroup)

--- devGroup/Desafio1.py
+++ devGroup/Desafio1.py
@@ -1,3 +1,24 @@
 # Construir um programa em Python que gera um cartão digital de desenvolvedor no terminal.
 # usando: variáveis, input(), conversão de tipos, operadores aritméticos, condicionais (if/elif/else) e f-strings.
 
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: (apenas números) "))
+linguagem_favorita = input("Digite sua linguagem de programação favorita: ")
+anos_estudados = int(input("Digite quantos anos você tem estudado programação: (apenas números) "))
+
+if anos_estudados == 0:
+    nivel_xp = 10 
+else:
+    nivel_xp = anos_estudados * 100
+
+
+print("\n" + "=" * 42)
+print("======= CARTÃO DIGITAL DE DESENVOLVEDOR =======")
+print("=" * 42)
+print(f"Nome: {nome}")
+print(f"Idade: {idade} anos")
+print(f"Linguagem Favorita: {linguagem_favorita}")
+print(f"Anos Estudados: {anos_estudados}")
+print(f"Nível de XP: {nivel_xp}")
+print("=" * 42)
+
